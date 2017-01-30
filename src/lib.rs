4b2ce396e17089787f9f5bf12cd5ce61b47ad5c3
Justin Uy <justin.uy@user.noreply.github.com>
2017-01-30 06:39:07 +0000

    Partially implemented VGA buffer

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,12 @@
-#![feature(lang_items)]
+#![feature(unique, lang_items, const_fn)]
 #![no_std]
 extern crate rlibc;
+extern crate volatile;
+mod vga_buffer;
 
 #[no_mangle]
 pub extern fn rust_main() {
-    let hello = b"Hello World!";
-    let color_byte = 0x1f; // white foreground, blue background
-
-    let mut hello_colored = [color_byte; 24];
-    for (i, char_byte) in hello.into_iter().enumerate() {
-        hello_colored[i*2] = *char_byte;
-    }
-
-    // write `Hello World!` to the center of the VGA text buffer
-    let buffer_ptr = (0xb8000 + 1988) as *mut _;
-    unsafe { *buffer_ptr = hello_colored };
+    vga_buffer::print_something();
 
     loop {}
 }
